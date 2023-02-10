f55f82706242036324c796834b632c8f32e939fb
Student <lab-103@aptech-msg.edu>
2023-02-11 01:46:21 +0500

    version-1

---
+++ Marinejs/marine.js
@@ -0,0 +1,49 @@
+// Body 3
+
+$(document).ready(function () {
+
+    $('.counter').each(function () {
+        $(this).prop('counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+});
+
+// Body 3
+
+
+// Body 4
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 1,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+// Body 4
